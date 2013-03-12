0aef1b4383f1f50938ca43b0ef269f6a2f5357b1
Tyler Tidman <tyler.tidman@draak.ca>
2013-03-12 17:03:29 -0400

    Major rework of the python photo processing script

--- photo.py
+++ photo.py
@@ -1,50 +1,120 @@
-import pyexiv2
-import sys
-import os
-#import datetime
+#!/usr/bin/python
 
-oldfilename = sys.argv[1]
-oldextension = os.path.splitext(oldfilename)[-1]
+# python-pyexiv2
+# python-magic (may need to use file instead)
 
-# Try to find the exif header.
-try:
-  metadata = pyexiv2.ImageMetadata(oldfilename)
-  metadata.read()
-except:
-  sys.exit('Unable to find a valid exif header.')
 
-# Extract the timestamp to use as a filename.
-try:
-  newbasename = metadata['Exif.Photo.DateTimeDigitized'].value.strftime('%Y-%m-%d-%H-%M-%S')
-except:
+def wow(old_photo_filename):
+  '''Poop'''
+
+  import pyexiv2
+  import sys
+  import os
+  import datetime
+
+
+  # Extract the exif header.
   try:
-    newbasename = metadata['Exif.Image.DateTime'].value.strftime('%Y-%m-%d-%H-%M-%S')
+    metadata = pyexiv2.ImageMetadata(old_photo_filename)
+    metadata.read()
   except:
-    sys.exit('Unable to find a valid exif timestamp.')
+    sys.exit('Unable to find a valid exif header in {}.' \
+      .format(old_photo_filename))
 
-try:
-  if metadata.mime_type == 'image/x-nikon-nef':
-    newextension = 'nef'
-  #else if metadata.mime_type == 'image/jpeg':
+
+  # Find the file type.
+  try:
+    a_type = metadata.mime_type
+  except:
+    #try:
+    # see if its a video file
+    #except:
+    sys.exit('Unable to determine image type for {}.' \
+      .format(old_photo_filename))
+
+  if a_type == 'image/x-nikon-nef':
+    new_photo_extension = '.nef'
+  #elif a_type == 'image/canon-something':
+  #  new_photo_extension = '.cr2'
+  elif a_type == 'image/jpeg':
+    new_photo_extension = '.jpg'
   else:
-    newextension = 'jpg'
-except:
-  sys.exit('Unable to determine image type from header.')
-
-# Output all the exif fields as text.
-try:
-  newfilename = newbasename + '.' + newextension
-
-  with open(newfilename + '.exif', 'w') as exiffile:
-    for key in sorted(metadata.exif_keys):
-      exiffile.write('{} = \'{}\'\n'.format(key, metadata[key].human_value))
-except:
-  sys.exit('Unable to open text file for writing.')
+    new_photo_extension = os.path.splitext(old_photo_filename)[-1].lower()
 
 
-#if __name__ == '__main__':
+  # Find a datetime string.
+  try:
+    a_photo_date = metadata['Exif.Photo.DateTimeDigitized'].value
+  except:
+    try:
+      a_photo_date = metadata['Exif.Image.DateTime'].value
+    except:
+      #try:
+      #  a_photo_date = datetime.datetime.fromtimestamp(
+      #                 os.stat(old_photo_filename).st_ctime)
+      #except:
+      sys.exit('Unable to find a valid timestamp in {}.' \
+        .format(old_photo_filename))
 
 
-#jhead -autorot *.jpg
-#jhead -nf%Y%m%d-%H%M%S *.jpg
+  # Work with the actual files.
+  try:
+    an_album_location = a_photo_date.strftime('%Y') + os.sep + \
+                        a_photo_date.strftime('%Y-%m-%d')
+    new_photo_basename = an_album_location + os.sep + \
+                         a_photo_date.strftime('%Y-%m-%d-%H-%M-%S')
+    new_photo_filename = new_photo_basename + new_photo_extension
+    new_exif_filename = new_photo_filename + '.txt'
+  except:
+    sys.exit('Unable to build a valid filename for {}' \
+      .format(old_photo_filename))
+
+  # Move the photo into the correct album.
+  try:
+    if not os.path.exists(new_photo_filename):
+      os.renames(old_photo_filename, new_photo_filename)
+    else:
+      new_photo_filename = new_photo_basename + 'a' + new_photo_extension
+      if not os.path.exists(new_photo_filename)
+        os.renames(old_photo_filename, new_photo_filename)
+      else:
+        new_photo_filename = new_photo_basename + 'b' + new_photo_extension
+        if not os.path.exists(new_photo_filename)
+          os.renames(old_photo_filename, new_photo_filename)
+        else:
+          sys.exit('Unable to rename {}'.format(old_photo_filename))
+  except:
+    sys.exit('Unable to move photo {}.'.format(old_photo_filename))
+
+  # Dump the exif fields.
+  try:
+    if not os.path.exists(new_exif_filename):
+      with open(new_exif_filename, 'wt') as an_exif_file:
+        for key in sorted(metadata.exif_keys):
+          an_exif_file.write('{} = {}\n' \
+            .format(key, metadata[key].human_value))
+  except:
+    sys.exit('Unable to write exif data {}.'.format(new_exif_filename))
+
+
+if __name__ == '__main__':
+  import sys
+
+  wow(sys.argv[1])
+
+
 #nice -n5 ufraw-batch --out-type=jpeg --overwrite */*.nef
+
+
+# case `jpegexiforient -n "$i"` in
+# 1) transform="";;
+# 2) transform="-flip horizontal";;
+# 3) transform="-rotate 180";;
+# 4) transform="-flip vertical";;
+# 5) transform="-transpose";;
+# 6) transform="-rotate 90";;
+# 7) transform="-transverse";;
+# 8) transform="-rotate 270";;
+# *) transform="";;
+# esac
+# jpegtran $transform
