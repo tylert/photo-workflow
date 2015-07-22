6da173957dfa8b364fb9c3d3be1b5097fa34bdc7
Tyler Tidman <tyler.tidman@draak.ca>
2015-07-22 16:24:21 -0400

    More pep8 improvements

--- photo.py
+++ photo.py
@@ -12,30 +12,30 @@ import datetime
 
 
 def rename_photo_and_dump_exif(old_photo_filename):
-  '''Extract info from the exif header to rename the file and move it'''
+    '''Extract info from the exif header to rename the file and move it'''
 
     # Extract the exif header.
     try:
         metadata = pyexiv2.ImageMetadata(old_photo_filename)
         metadata.read()
     except:
-        sys.exit('Unable to find a valid exif header in {}.' \
-            .format(old_photo_filename))
+        sys.exit('Unable to find a valid exif header in {}.'
+                 .format(old_photo_filename))
 
     # Find the file type.
     try:
         a_type = metadata.mime_type
     except:
-      #try:
-      #    see if its a video file
-      #except:
-      sys.exit('Unable to determine image type for {}.' \
-          .format(old_photo_filename))
+        # try:
+        #     see if its a video file
+        # except:
+        sys.exit('Unable to determine image type for {}.'
+                 .format(old_photo_filename))
 
     if a_type == 'image/x-nikon-nef':
         new_photo_extension = '.nef'
-    #elif a_type == 'image/canon-something':
-    #    new_photo_extension = '.cr2'
+    # elif a_type == 'image/canon-something':
+    #     new_photo_extension = '.cr2'
     elif a_type == 'image/jpeg':
         new_photo_extension = '.jpg'
     else:
@@ -48,27 +48,27 @@ def rename_photo_and_dump_exif(old_photo_filename):
         try:
             a_photo_date = metadata['Exif.Image.DateTime'].value
         except:
-          #try:
-          #    a_photo_date = datetime.datetime.fromtimestamp(
-          #                   os.stat(old_photo_filename).st_ctime)
-          #except:
-          sys.exit('Unable to find a valid timestamp in {}.' \
-              .format(old_photo_filename))
+            # try:
+            #     a_photo_date = datetime.datetime.fromtimestamp(
+            #                    os.stat(old_photo_filename).st_ctime)
+            # except:
+            sys.exit('Unable to find a valid timestamp in {}.'
+                     .format(old_photo_filename))
 
     # Work with the actual files.
     try:
         an_album_location = a_photo_date.strftime('%Y') + os.sep + \
-                            a_photo_date.strftime('%Y-%m-%d')
+            a_photo_date.strftime('%Y-%m-%d')
         new_photo_basename = an_album_location + os.sep + \
-                             a_photo_date.strftime('%Y-%m-%d-%H-%M-%S')
+            a_photo_date.strftime('%Y-%m-%d-%H-%M-%S')
         new_photo_filename = new_photo_basename + new_photo_extension
         new_exif_filename = new_photo_filename + '.txt'
 
         if not os.path.exists(an_album_location):
             os.makedirs(an_album_location)
     except:
-        sys.exit('Unable to build a valid filename for {}' \
-            .format(old_photo_filename))
+        sys.exit('Unable to build a valid filename for {}'
+                 .format(old_photo_filename))
 
     # Move the photo into the correct album.
     try:
@@ -79,7 +79,8 @@ def rename_photo_and_dump_exif(old_photo_filename):
             if not os.path.exists(new_photo_filename):
                 shutil.move(old_photo_filename, new_photo_filename)
             else:
-                new_photo_filename = new_photo_basename + 'b' + new_photo_extension
+                new_photo_filename = new_photo_basename + 'b' + \
+                    new_photo_extension
                 if not os.path.exists(new_photo_filename):
                     shutil.move(old_photo_filename, new_photo_filename)
                 else:
@@ -92,8 +93,8 @@ def rename_photo_and_dump_exif(old_photo_filename):
         if not os.path.exists(new_exif_filename):
             with open(new_exif_filename, 'wt') as an_exif_file:
                 for key in sorted(metadata.exif_keys):
-                    an_exif_file.write('{} = {}\n' \
-                        .format(key, metadata[key].human_value))
+                    an_exif_file.write('{} = {}\n'
+                                       .format(key, metadata[key].human_value))
     except:
         sys.exit('Unable to write exif data {}.'.format(new_exif_filename))
 
@@ -104,7 +105,7 @@ if __name__ == '__main__':
     rename_photo_and_dump_exif(sys.argv[1])
 
 
-#nice -n5 ufraw-batch --out-type=jpeg --overwrite */*.nef
+# nice -n5 ufraw-batch --out-type=jpeg --overwrite */*.nef
 
 
 # case `jpegexiforient -n "$i"` in
