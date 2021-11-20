17d1cd76162c29e9af9f50f7da041fa3622c2753
Tyler Tidman <tyler.tidman@draak.ca>
2021-11-20 15:34:14 -0500

    Try to use raw bash+rename for photo processing

--- process_stuff.sh
+++ process_stuff.sh
@@ -5,7 +5,8 @@ find . -type d -print0 | xargs -0 chmod 0755
 find . -type f -print0 | xargs -0 chmod 0644
 
 # Fix filenames
-find . -name '*.jpg' | xargs exiv2 -r '%Y-%m-%d-%H-%M-%S' -F -v rename
+# find . -name '*.jpg' | xargs exiv2 -r '%Y-%m-%d-%H-%M-%S' -F -v rename
+rename 's/IMG_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)/$1-$2-$3-$4-$5-$6/g' *.jpg
 rename 's/VID_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)/$1-$2-$3-$4-$5-$6/g' *.mp4
 
 # Create directory structure and move stuff under it
