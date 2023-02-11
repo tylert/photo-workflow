df248067bf320b8f8e31cd4918bfa9e1c7e42de1
Tyler Tidman <tyler.tidman@draak.ca>
2023-02-11 13:05:54 -0500

    Maybe don't need the extra renames since jhead does all the jpegs

--- process_stuff.sh
+++ process_stuff.sh
@@ -5,7 +5,7 @@ find . -type d -print0 | xargs -0 chmod 0755
 find . -type f -print0 | xargs -0 chmod 0644
 
 # Fix filenames
-perl-rename 's/IMG_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)/$1-$2-$3-$4-$5-$6/g' *.jpg
+# perl-rename 's/IMG_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)/$1-$2-$3-$4-$5-$6/g' *.jpg
 perl-rename 's/VID_(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)/$1-$2-$3-$4-$5-$6/g' *.mp4
 # find . -name '*.jpg' | xargs exiv2 -r '%Y-%m-%d-%H-%M-%S' -F -v rename
 jhead -n%Y-%m-%d-%H-%M-%S *.jpg
